7b39be5f15459c133319a3883e965448ce6f0c73
futures1mple <alizadeh.ja217@gmail.com>
2021-07-21 22:17:00 +0400

    some design fixes

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -31,27 +31,27 @@ const Game: React.FC<GameProps> = () => {
 
 
 
-    const generateNumber = () => {
-        var fullNumber = [];
-        var digit1 = Math.floor(Math.random() * 10);
-        while(digit1 === 0) {
-            digit1 = Math.floor(Math.random() * 10);
-        }
-        var digit2 = Math.floor(Math.random() * 10);
-        while (digit1 === digit2) {
-            digit2 = Math.floor(Math.random() * 10);
-        }
-        var digit3 = Math.floor(Math.random() * 10);
-        while (digit3 === digit1 || digit3 === digit2) {
-            digit3 = Math.floor(Math.random() * 10);
-        }
-        var digit4 = Math.floor(Math.random() * 10);
-        while (digit4 === digit1 || digit4 === digit2 || digit4 === digit3) {
-            digit4 = Math.floor(Math.random() * 10);
-        }
-        fullNumber.push(digit1, digit2, digit3, digit4);
-        ourNumber = parseInt(fullNumber.join(""));
-    };
+    // const generateNumber = () => {
+    //     var fullNumber = [];
+    //     var digit1 = Math.floor(Math.random() * 10);
+    //     while(digit1 === 0) {
+    //         digit1 = Math.floor(Math.random() * 10);
+    //     }
+    //     var digit2 = Math.floor(Math.random() * 10);
+    //     while (digit1 === digit2) {
+    //         digit2 = Math.floor(Math.random() * 10);
+    //     }
+    //     var digit3 = Math.floor(Math.random() * 10);
+    //     while (digit3 === digit1 || digit3 === digit2) {
+    //         digit3 = Math.floor(Math.random() * 10);
+    //     }
+    //     var digit4 = Math.floor(Math.random() * 10);
+    //     while (digit4 === digit1 || digit4 === digit2 || digit4 === digit3) {
+    //         digit4 = Math.floor(Math.random() * 10);
+    //     }
+    //     fullNumber.push(digit1, digit2, digit3, digit4);
+    //     ourNumber = parseInt(fullNumber.join(""));
+    // };
 
     const numberCheck = () => {
         let son = ourNumber.toString().split("");
@@ -84,60 +84,65 @@ const Game: React.FC<GameProps> = () => {
     return (
         <div className="Game">
             <div className="container d-flex flex-wrap">
-                <div id="game-board" className="game-board rules-container col-12 col-md-6">
-                    <ul>
-                        <li>The number to be guessed must be a 4 digit number, using only digits from 0 - 9, each digit atmost once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.</li>
-                        <li>For every guess that the player makes, he gets 2 values - the number of bulls and the number of cows. 1 bull means the guess contains and the target number have 1 digit in common, and in the correct position. 1 cow means the guess and the target have 1 digit in common, but not in correct position. e.g. Let the target be 1234. Guessing 4321 will give 0 bulls and 4 cows. 3241 will give 1 bull and 3 cows. 4 bulls means you have won the game!</li>
-                    </ul>
-                </div>
-                <div
-                id="game-board"
-                className="game-board col-12 col-md-6"
-                style={{ flexWrap: "wrap" }}
-                >
-                    <div className="col-12 mb-5">
-                        You can generate new number. Just push the button!
-                        <button
-                        onClick={() => {
-                            generateNumber();
-                            window.location.reload();
-                        }}
-                        className="col-12 mt-3 p-1 generate-button"
-                        style={{ border: "none" }}
-                        >
-                        Generate
-                        </button>
-                    </div>
-                    <div className="col-12">Enter your predicted number:</div>
-                    <div className="mt-3 justify-content-between col-12">
-                        <input
-                            type="number col-7"
-                            onChange={(e) => {
-                                setUserNumber(e.target.value);
-                            }}
-                            className="user-input-field"
-                            />
-                        <button
-                            className="user-button offset-1 col-4"
-                            onClick={(e) => {
-                                numberCheck();
-                                console.log(userNumber);
-                                console.log(ourNumber);
-                            }}
-                            >
-                            Submit
-                        </button>
-                    </div>
-                    <div className="col-6 offset-3 align-items-center mt-5">
-                        <div className="col-12 text-center">Your numbers</div>
-                        <div className="col-12 number-list">
+                <div className="row">
+                    <div className="col-12 col-md-6 p-3">
+                        <div id="game-board" className="game-board rules-container">
                             <ul>
-                                {numberList.map((number)=> {
-                                    return(<li style={{listStyleType:'none'}}>{number}</li>)
-                                })}
+                                <li>The number to be guessed must be a 4 digit number, using only digits from 0 - 9, each digit atmost once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.</li>
+                                <li>For every guess that the player makes, he gets 2 values - the number of bulls and the number of cows. 1 bull means the guess contains and the target number have 1 digit in common, and in the correct position. 1 cow means the guess and the target have 1 digit in common, but not in correct position. e.g. Let the target be 1234. Guessing 4321 will give 0 bulls and 4 cows. 3241 will give 1 bull and 3 cows. 4 bulls means you have won the game!</li>
                             </ul>
                         </div>
                     </div>
+                    <div className="col-12 col-md-6 p-3">                
+                        <div
+                        id="game-board"
+                        className="game-board"
+                        style={{ flexWrap: "wrap" }}
+                        >
+                            <div className="col-12 mb-5">
+                                You can generate new number. Just push the button!
+                                <button
+                                onClick={() => {
+                                    // generateNumber();
+                                    window.location.reload();
+                                }}
+                                className="col-12 mt-3 p-1 generate-button"
+                                style={{ border: "none" }}
+                                >
+                                Generate
+                                </button>
+                            </div>
+                            <div className="col-12">Enter your predicted number:</div>
+                            <div className="mt-3 justify-content-between col-12">
+                                <input
+                                    type="number col-7"
+                                    onChange={(e) => {
+                                        setUserNumber(e.target.value);
+                                    }}
+                                    className="user-input-field"
+                                    />
+                                <button
+                                    className="user-button offset-1 col-4"
+                                    onClick={(e) => {
+                                        numberCheck();
+                                    }}
+                                    >
+                                    Submit
+                                </button>
+                            </div>
+                            <div className="col-6 offset-3 align-items-center mt-5">
+                                <div className="col-12 text-center">Your numbers</div>
+                                <div className="col-12 number-list">
+                                    <ul>
+                                        {numberList.map((number)=> {
+                                            return(<li style={{listStyleType:'none'}}>{number}</li>)
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
                 <div id='win-banner' className="win-banner d-none">
                     <div className="win-text" style={{ fontSize: "3rem" }}>
@@ -149,7 +154,7 @@ const Game: React.FC<GameProps> = () => {
                         onClick={() => {
                             document.getElementById("game-board")?.classList.remove("d-none");
                             document.getElementById("win-banner")?.classList.add("d-none");
-                            generateNumber();
+                            // generateNumber();
                             window.location.reload();
                         }}
                     >
