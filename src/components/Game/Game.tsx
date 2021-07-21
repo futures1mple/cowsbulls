3c7bf61071fee8a17bc8ff63b5fa99b878154bd8
futures1mple <alizadeh.ja217@gmail.com>
2021-07-21 10:30:14 +0400

    rules

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -18,19 +18,26 @@ fullNumber.push(digit1, digit2, digit3);
 ourNumber = parseInt(fullNumber.join(""));
 
 const Game: React.FC<GameProps> = () => {
-    const [userNumber, setUserNumber] = useState("");
-    const [numberList, setNumberList] = useState(['']);
+    const [ userNumber , setUserNumber ] = useState("");
+    const [ numberList , setNumberList ] = useState(['']);
+    const [ pace , setPace ] = useState(0);
+
+
 
     const generateNumber = () => {
         var fullNumber = [];
         var digit1 = Math.floor(Math.random() * 9) + 1;
         var digit2 = Math.floor(Math.random() * 9) + 1;
         while (digit1 === digit2) {
-        digit2 = Math.floor(Math.random() * 9) + 1;
+            digit2 = Math.floor(Math.random() * 9) + 1;
         }
         var digit3 = Math.floor(Math.random() * 9) + 1;
         while (digit3 === digit1 || digit3 === digit2) {
-        digit3 = Math.floor(Math.random() * 9) + 1;
+            digit3 = Math.floor(Math.random() * 9) + 1;
+        }
+        var digit4 = Math.floor(Math.random() * 9) + 1;
+        while (digit4 === digit1 || digit4 === digit2 || digit4 === digit3) {
+            digit4 = Math.floor(Math.random() * 9) + 1;
         }
         fullNumber.push(digit1, digit2, digit3);
         ourNumber = parseInt(fullNumber.join(""));
@@ -42,8 +49,8 @@ const Game: React.FC<GameProps> = () => {
         b = userNumber.split("");
         var cows = 0;
         var bulls = 0;
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 4; j++) {
                 if (b[i] === son[j]) {
                     cows++;
                     if (i === j) {
@@ -53,7 +60,7 @@ const Game: React.FC<GameProps> = () => {
                 }
             }
         }
-
+        setPace(pace+1)
         setNumberList([...numberList, userNumber + " "  + " – " + " " + "cows:  " + cows + " " + "; " + " " + "bulls:  " + bulls])
 
         
@@ -66,10 +73,17 @@ const Game: React.FC<GameProps> = () => {
 
     return (
         <div className="Game">
-            <div className="container">
+            <div className="container d-flex flex-wrap">
+                <div id="game-board" className="game-board rules-container col-6">
+                    <ul>
+                        <li>The number to be guessed must be a 4 digit number, using only digits from 1 - 9, each digit atmost once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.</li>
+                        <li>For every guess that the player makes, he gets 2 values - the number of bulls and the number of cows. 1 bull means the guess contains and the target number have 1 digit in common, and in the correct position. 1 cow means the guess and the target have 1 digit in common, but not in correct position. e.g. Let the target be 1234. Guessing 4321 will give 0 bulls and 4 cows. 3241 will give 1 bull and 3 cows. 4 bulls means you have won the game!</li>
+                        <li>The player gets 7 chances to correctly guess the entire number.</li>
+                    </ul>
+                </div>
                 <div
                 id="game-board"
-                className="game-board col-6 offset-3"
+                className="game-board col-6"
                 style={{ flexWrap: "wrap" }}
                 >
                     <div className="col-12 mb-5">
@@ -77,6 +91,7 @@ const Game: React.FC<GameProps> = () => {
                         <button
                         onClick={() => {
                             generateNumber();
+                            window.location.reload();
                         }}
                         className="col-12 mt-3 p-1 generate-button"
                         style={{ border: "none" }}
@@ -117,6 +132,7 @@ const Game: React.FC<GameProps> = () => {
                 <div id='win-banner' className="win-banner d-none">
                     <div className="win-text" style={{ fontSize: "3rem" }}>
                         You win!
+                        <div className="pace">You did it in {pace} steps</div>
                     </div>
                     <button
                         className="play-again p-2 mt-3"
@@ -124,6 +140,7 @@ const Game: React.FC<GameProps> = () => {
                             document.getElementById("game-board")?.classList.remove("d-none");
                             document.getElementById("win-banner")?.classList.add("d-none");
                             generateNumber();
+                            window.location.reload();
                         }}
                     >
                         Play again!
